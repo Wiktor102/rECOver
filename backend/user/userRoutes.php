ab338df17bcb711e4938c91734a1091159767b02
Wiktor Golicz <wiktorgolicz1@gmail.com>
2024-11-13 22:19:15 +0100

    Add php for updating user

--- backend/user/userRoutes.php
+++ backend/user/userRoutes.php
@@ -26,3 +26,60 @@ $router->addRoute("GET", "/user", function () {
         $conn->close();
     }
 });
+
+$router->addRoute("PATCH", "/user", function () {
+    global $credentials;
+    global $inputJson;
+    $userId = $credentials["userId"];
+
+    if (!isset($inputJson) || $inputJson === null) {
+        http_response_code(400);
+        exit();
+    }
+
+    $allowedKeys = ["tags", "streaks", "quizQuestions"];
+    foreach ($inputJson as $key => $value) {
+        if (!in_array($key, $allowedKeys)) {
+            echo json_encode(["error" => "Invalid key: $key"]);
+            return;
+        }
+    }
+
+    $conn = connect();
+    $stmt = $conn->stmt_init();
+
+    try {
+        $setClause = [];
+        $params = [];
+        $types = "";
+
+        foreach ($inputJson as $key => $value) {
+            $setClause[] = "`$key` = ?";
+            $params[] = is_array($value) ?  json_encode($value) : $value;
+            $types .= is_int($value) ? "i" : "s";
+        }
+
+        $params[] = $userId;
+        $types .= "i";
+
+        $sql = "UPDATE `users` SET " . implode(", ", $setClause) . " WHERE `id` = ?;";
+
+        $stmt->prepare($sql);
+        $stmt->bind_param($types, ...$params);
+        $stmt->execute();
+
+        if ($stmt->affected_rows === 0) {
+            http_response_code(404);
+            echo json_encode(["error" => "No rows updated"]);
+            return;
+        }
+
+        http_response_code(204);
+    } catch (Exception $e) {
+        http_response_code(500);
+        echo json_encode(["error" => $e->getMessage()]);
+    } finally {
+        $stmt->close();
+        $conn->close();
+    }
+});
